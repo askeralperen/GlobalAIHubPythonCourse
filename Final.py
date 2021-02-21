38f94958dab91badbee4380301e0b536d5dd7577
askeralperen <aae.aydin@gmail.com>
2021-02-21 22:36:43 +0300

    Add files via upload

---
+++ Final.py
@@ -0,0 +1,50 @@
+# user: Alperen Aydın
+# mail: aae.aydin@gmail.com
+
+# HW Final
+
+class Recipe():
+  def __init__(self,products):
+    self.products = products
+
+  def cook(self):
+    #time = self.calc_time()
+    for product in self.products:
+      print(product[0],"kullanılıyor")
+      for i in range(product[1]):
+        print(i+1,"dk geçti")
+    time = self.calc_time()
+    print("yemeğiniz ",time, "dakikada hazırlandı.. Afiyet olsun: ")
+    
+  def calc_time(self):
+    time = 0
+    for product in self.products:
+      time += product[1]
+    return time
+
+class Recipe1(Recipe):
+  def __init__(self,products):
+    super().__init__(products)
+
+class Recipe2(Recipe):
+  def __init__(self,products):
+    super().__init__(products)
+
+class Recipe3(Recipe):
+  def __init__(self,products):
+    super().__init__(products)
+
+
+Products =[["Yumurta",3],["Süt",1],["Tereyağı",1],["Tuz",1],["Un",12]]
+
+recipe1 = Recipe1(Products)    #  parent >>> recipe1 = Recipe(Products)
+
+recipe1.cook()
+
+recipe2 = Recipe2(Products[2:])    #  parent >>> recipe1 = Recipe(Products)
+
+recipe2.cook()
+
+recipe3 = Recipe3(Products[:3])    #  parent >>> recipe1 = Recipe(Products)
+
+recipe3.cook()
